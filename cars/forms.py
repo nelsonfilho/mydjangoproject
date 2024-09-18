7e28a2912f378258a9c1f0416fabe2382db1315d
Nelson Pinto <nelson.lapf@gmail.com>
2024-09-18 09:47:15 -0300

    Adding validation to our form

--- cars/forms.py
+++ cars/forms.py
@@ -1,25 +1,15 @@
+from typing import Any
 from django import forms
 from cars.models import Brand, Car
 
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    plate = forms.CharField(max_length=7)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    value = forms.FloatField()
-    photo = forms.ImageField()
+class CarForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
 
-    def save(self):
-        car = Car(
-            model = self.cleaned_data['model'],
-            brand = self.cleaned_data['brand'],
-            factory_year = self.cleaned_data['factory_year'],
-            model_year = self.cleaned_data['model_year'],
-            plate = self.cleaned_data['plate'],
-            value = self.cleaned_data['value'],
-            photo = self.cleaned_data['photo'],
-        )
-        car.save()
-        return car
+    def clean_value(self) -> dict[str, Any]:
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('Valor minimo do carro deve ser de R$20.000')
+        return value
